20d7f7d4c1039dfcd8b3f2afd8c4756581b7d779
MarioVio1 <gattom22@gmail.com>
2025-05-08 17:35:16 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://mvajro-strava.hf.space"
+PASSWORD = "Sally"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
