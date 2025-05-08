dc9f715361cf8cfd7a403d16e841711cb9bce36d
MarioVio1 <gattom22@gmail.com>
2025-05-08 17:36:41 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://mvajro-strava.hf.space"
+PASSWORD = "Sally"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
